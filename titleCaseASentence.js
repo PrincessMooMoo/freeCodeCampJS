4d10baeeb88c881e6b1567c14b2cccd34060e9cf
Wendy C <chriscowendy@gmail.com>
2020-11-22 16:09:20 -0800

    trying to make the program work

--- titleCaseASentence.js
+++ titleCaseASentence.js
@@ -1,11 +1,19 @@
 function titleCase(str) {
   let lowerCase = str.toLowerCase();
-  console.log(lowerCase)
+  // console.log(lowerCase)
   let wordArr = lowerCase.split(" ");
-  console.log(wordArr)
-  wordArr.forEach(word) {
-    console.log(word);
-  };
+  for (let i = 0; i < wordArr.length; i++){
+    for (let j = 0; j < wordArr.length; j++){
+      j = j.toUpperCase;//work on this line later
+    }
+  }
+  // wordArr.forEach(word => 
+  //   word.forEach(letter => letter[0].toUpperCase));
+  // wordArr.replace()
+  // console.log(wordArr)
+  // let firstLetterArr = wordArr.map(i => i);
+  // console.log(firstLetterArr);
+  
   return str;
 }
 
