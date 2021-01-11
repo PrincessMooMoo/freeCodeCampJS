e3739b15a9ca57020c67729c1d059f844952c868
Wendy C <chriscowendy@gmail.com>
2021-01-10 20:23:08 -0800

    Added another example to whereDoIBelong.js
    Started up a new file: mutation.js

--- whereDoIBelong.js
+++ whereDoIBelong.js
@@ -20,3 +20,17 @@ function getIndexToIns(arr, num) {
 }
 
 console.log(getIndexToIns([2, 5, 10], 15))
+
+// example from freecodecamp
+
+// function getIndexToIns(arr, num) {
+//   arr.sort(function(a, b) {
+//     return a - b;
+//   });
+
+//   for (var a = 0; a < arr.length; a++) {
+//     if (arr[a] >= num) return a;
+//   }
+
+//   return arr.length;
+// }
