7f402469961d81257d93803f6d8f3291d2bbff62
Wendy C <chriscowendy@gmail.com>
2021-01-10 19:05:53 -0800

    I created this algorithm myself!

--- whereDoIBelong.js
+++ whereDoIBelong.js
@@ -1,8 +1,22 @@
 function getIndexToIns(arr, num) {
-    const newArray = arr.slice();
-    newArray.sort(function(a, b){return a-b})
-    //console.log(newArray)
-    return num;
+  const newArray = arr.slice();
+  newArray.sort(function(a, b){return a-b})
+  console.log(newArray)
+  const indexZero = 0;
+  if(newArray && newArray.length > 0) {
+    
+    for(let i = 0; i < newArray.length; i++){
+      if (num <= newArray[i]) {
+        return (i);
+      }
+      if (num > newArray[newArray.length-1]){
+        return newArray.length;
+      }
+    }
+
+  } else {
+    return indexZero;
   }
-  
-  getIndexToIns([3, 10, 5], 3)
+}
+
+console.log(getIndexToIns([2, 5, 10], 15))
